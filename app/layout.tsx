5d8477c1c9ffbf686613d8b81ab7157c0da39c6e
shashikant <shashikant040203@gmail.com>
2025-01-16 19:19:49 +0530

    gtag has been added to layout.tsx prometheus.yml has been addedfor prometheus setup matrics has been added in prometheus

--- app/layout.tsx
+++ app/layout.tsx
@@ -167,7 +167,30 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${localInter.variable}`}
     >
+      <head>
+      <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){
+          w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PQGGRR2Z');</script>
+      <!-- End Google Tag Manager -->
+
+      </head>
+
+
       <body className="min-h-screen bg-black font-sans antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PQGGRR2Z"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ThemeProvider
             attribute="class"
             defaultTheme="dark"
